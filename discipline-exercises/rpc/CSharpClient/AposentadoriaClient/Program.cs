154519cd1db8694901a543ac9e3c2c4fb3a1b096
ViniciusSoares1943 <vinicius.soares.01@hotmail.com>
2024-10-14 19:55:14 -0300

    Adicionando exercicio aposentadoria usando RPC

---
+++ discipline-exercises/rpc/CSharpClient/AposentadoriaClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Aposentadoria;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:50051");
+
+        var client = new AposentadoriaService.AposentadoriaServiceClient(channel);
+
+        while (true)
+        {
+            Console.WriteLine("Digite a idade do funcionário: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o tempo de serviço do funcionário: ");
+            int tempoServico = int.Parse(Console.ReadLine());
+
+            var request = new AposentadoriaRequest
+            {
+                Idade = idade,
+                TempoServico = tempoServico
+            };
+
+            var response = await client.VerificarAposentadoriaAsync(request);
+
+            Console.WriteLine($"Resultado: {response.Mensagem}");
+            Console.WriteLine("\nPara finalizar use ctrl + C");
+        }
+
+    }
+}
