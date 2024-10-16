1a169e50e93329a80c35bc1acb9c56faa900b24b
Rafael Melo <108890549+rafamelo01@users.noreply.github.com>
2024-10-16 11:33:31 -0300

    Delete discipline-exercises/rmi directory

--- discipline-exercises/rmi/MaioridadeService.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MaioridadeService extends Remote {
-    String verificarMaioridade(String nome, String sexo, int idade) throws RemoteException;
-}
